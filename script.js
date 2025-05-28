9a62ea1575a2d36aa920122c214a2addf2c16253
Neeraj Kumar <neerajkumar090102@gmail.com>
2025-05-28 17:04:16 +0530

    28may

--- script.js
+++ script.js
@@ -695,3 +695,17 @@ data.forEach(row => {
   `;
   tableBody.appendChild(tr);
 });
+
+
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+
+window.addEventListener("mousemove", function (e) {
+  const posX = e.clientX;
+  const posY = e.clientY;
+  cursorDot.style.left = `${posX}px`;
+  cursorDot.style.top = `${posY}px`;
+  cursorOutline.style.left = `${posX}px`;
+  cursorOutline.style.top = `${posY}px`;
+
+});
